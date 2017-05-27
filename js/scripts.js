9ca40715a1efe453ec89c11e44efe0629cce083c
iJustinus <justinushermawan@gmail.com>
2017-05-27 13:05:23 +0700

    :rocket: Update 27 Mei

--- js/scripts.js
+++ js/scripts.js
@@ -43,21 +43,22 @@ function processData() {
   reader.onload = function() {
     var _data = this.result;
     var _arrData = CSVToArray(_data);
-
+	
     // Remove header
     _arrData.splice(0, 1);
-
+	  _arrData.splice(_arrData.length - 1, 1);
+	
     var _miu0 = parseInt(document.getElementById('miu0').value);
     var _interval = parseInt(document.getElementById('interval').value);
 
     var _ct = 0;
     for (var i = 1; i <= _arrData.length; i++) {
       _arrData[i - 1][0] = parseInt(_arrData[i - 1][0]);
-      _arrData[i - 1][1] = parseInt(_arrData[i - 1][1]);
+      _arrData[i - 1][1] = _arrData[i - 1][1];
       _arrData[i - 1][2] = _miu0;
 
-      var _brix = parseInt(_arrData[i - 1][1]);
-      if (_brix < _arrData[i - 1][2]) _arrData[i - 1][3] = 0;
+      var _brix = _arrData[i - 1][1];
+      if (_brix <= _arrData[i - 1][2]) _arrData[i - 1][3] = 0;
       else _arrData[i - 1][3] = 1;
 
       _ct += _arrData[i - 1][3];
@@ -88,7 +89,52 @@ function processData() {
       return x[4] !== -1;
     });
 
-    alert("Data sesuai Excel:\n\n" + _arrData.join("\n"));
+    // Start counting EWMA
+    var _lambda = 0.9;
+    var _arrNewData = [];
+    for (var i = 0; i < _arrData.length; i++) {
+      _arrNewData[i] = [];
+      _arrNewData[i][0] = _lambda; // Lambda
+      _arrNewData[i][1] = _arrData[i][4]; // Si Brix
+      _arrNewData[i][2] = _lambda * _arrData[i][4]; // Lambda * Si
+      _arrNewData[i][3] = (1 - _lambda).toFixed(1); // 1 - Lambda
+      _arrNewData[i][4] = _arrNewData[i][1];
+    }
+
+    var temp = [];
+    for (var i = 0; i < _arrNewData.length; i++) {
+      temp.push(_arrNewData[i][1]);
+    }
+
+    for (var i = 0; i < _arrNewData.length; i++) {
+      if (i > 0) _arrNewData[i][4] = temp[i - 1]; // Si - 1
+      _arrNewData[i][5] = _arrNewData[i][3] * _arrNewData[i][4]; // (1 - lambda) * (Si - 1)
+    }
+
+    var _sumOfSiBrix = 0;
+    for (var i = 0; i < _arrNewData.length; i++) {
+      _sumOfSiBrix += _arrNewData[i][1];
+    }
+    _arrNewData[0][6] = (_sumOfSiBrix * 1.0 / _arrNewData.length).toFixed(6); // First EWMASi
+
+    for (var i = 1; i < _arrNewData.length; i++) {
+      _arrNewData[i][6] = _arrNewData[i][2] + _arrNewData[i][5]; // EWMASi
+    }
+
+    for (var i = 0; i < _arrNewData.length; i++) {
+      _arrNewData[i][7] = _interval; // n
+      _arrNewData[i][8] = _p.toFixed(6); // p
+      _arrNewData[i][9] = 1.96; // k
+      _arrNewData[i][10] = (_arrNewData[i][7] * _arrNewData[i][8]) + (_arrNewData[i][9] * Math.sqrt( ( _lambda / (2 - _lambda) ) * (_arrNewData[i][7] * _arrNewData[i][8]) * (1 - _arrNewData[i][8]) ));
+      _arrNewData[i][10] = _arrNewData[i][10].toFixed(4); // UCL
+      _arrNewData[i][11] = _arrNewData[i][7] * _arrNewData[i][8]; // CL
+      _arrNewData[i][12] = (_arrNewData[i][7] * _arrNewData[i][8]) - (_arrNewData[i][9] * Math.sqrt( ( _lambda / (2 - _lambda) ) * (_arrNewData[i][7] * _arrNewData[i][8]) * (1 - _arrNewData[i][8]) ));
+      _arrNewData[i][12] = _arrNewData[i][12].toFixed(5); // LCL
+      _arrNewData[i][13] = (_arrNewData[i][6] > _arrNewData[i][10]) ? "Out" : (_arrNewData[i][6] < _arrNewData[i][12]) ? "Out" : "In";
+    }
+
+    console.log(_arrNewData.join("\n"));
+    alert("Data sesuai Excel:\n\n" + _arrNewData.join("\n"));
   };
   reader.readAsText(file);
 }
